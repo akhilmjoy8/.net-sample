a9084185558c400c5218934002b209649d23f6ae
akhilmjoy8 <akhilmjoy8@gmail.com>
2019-02-24 17:16:36 +0530

    first commit

---
+++ SampleProject/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using SampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        LoginRepository LogRepo;
+        public HomeController()
+        {
+            LogRepo = new LoginRepository();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register(int? isreg)
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Register(tbl_Login model)
+        {
+            LogRepo.Register(model);
+            return RedirectToAction("Register",new { isreg = 1});
+        }
+        public ActionResult Details()
+        {
+            List<tbl_Login> model = new List<tbl_Login>();
+            model = LogRepo.GetDetails();
+            return View(model);
+        }
+    }
+}
---
+++ SampleProject/Models/LoginRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleProject.Models
+{
+    public class LoginRepository
+    {
+        SampleProjectEntities dbContext = new SampleProjectEntities();        
+               
+        internal bool Register(tbl_Login model)
+        {
+            dbContext.AddTotbl_Login(model);
+            dbContext.SaveChanges();
+            return true;
+        }
+        internal List<tbl_Login> GetDetails()
+        {
+            return dbContext.tbl_Login.ToList();
+        }
+    }
+}
